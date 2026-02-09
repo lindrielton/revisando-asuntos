8aff8dd6794a4b7468ffcadf4e29b17460c7a3c9
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-02-09 07:04:55 -0300

    exercicio guiado

---
+++ revisando/11-exercicio-calc/index.js
@@ -0,0 +1,17 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+input.addEventListener('keydown', function(ev){
+    ev.preventDefault()
+    if(allowedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }
+
+    if
+})
