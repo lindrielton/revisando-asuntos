be1c0c83a12e70e2fd45550bd781cf13e6978d48
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-02-09 22:35:28 -0300

    finalizando ex

--- revisando/11-exercicio-calc/index.js
+++ revisando/11-exercicio-calc/index.js
@@ -6,6 +6,27 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+
+document.querySelectorAll('.charKey').forEach(function(chakeyBtn){
+    chakeyBtn.addEventListener('click', function(){
+        const value = chakeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+
+
+
+
+document.getElementById('clear').addEventListener('click' , function(){
+    input.value = ''
+    input.focus()
+
+})
+
+
+
+
 input.addEventListener('keydown', function(ev){
     ev.preventDefault()
     if(allowedKeys.includes(ev.key)){
@@ -13,5 +34,49 @@ input.addEventListener('keydown', function(ev){
         return
     }
 
-    if
+    if(ev.key === 'Backspace'){
+        input.value = input.value.slice(0, -1)
+    }
+
+    if(ev.key = 'Enter'){
+        calculate()
+    }
+})
+
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+
+function calculate(){
+    const result = eval(input.value)
+    resultInput.value = result
+    
+}
+
+document.getElementById('copyToClipboard').addEventListener('click', function(ev){
+    const button = ev.currentTarget 
+    if(button.innerText === 'Copy'){
+        button.innerText = 'Copied!'
+        button.ClassList.add('success')
+        navigator.clipboard.writeText(resultInput.value)
+    }
+})
+
+
+document.getElementById('themeSwitcher').addEventListener('click', function(){
+    if(main.dataset.theme === 'dark'){
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('-font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'ligth'
+
+    }else{
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('-font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+
+    }
 })
