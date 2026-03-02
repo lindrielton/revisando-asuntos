e31a7169fba189be1b13e71174f02c53e47b4c29
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-03-02 06:14:21 -0300

    finalizando desafio

--- revisando/14-cadastro-Devs/projeto/devs.js
+++ revisando/14-cadastro-Devs/projeto/devs.js
@@ -23,3 +23,120 @@ funcionamento você pode utilizar um simples console.log()
 
 Para esse exercício você deve utilizar apenas eventos adicionados via javascript
  e a página deve funcionar sem acionar um recarregamento.*/
+
+
+function creatLabel(forname, name){
+    let label = document.createElement("label")
+    label.forname = forname;
+    label.textContent = name
+    return label;
+
+}
+
+function creatInput( id, value, name,  placeholder = '', type = 'text',){
+      let input = document.createElement("input")
+      input.id = id;
+      input.value = value;
+      input.name = name;
+      input.placeholder = placeholder;
+      input.type = type;
+      return input
+}
+
+
+let bunttonClic = document.getElementById('btn')
+let arealist = document.getElementById("areaList")
+let cont = 0
+let devs = []
+
+
+
+bunttonClic.addEventListener("click", function(ev){
+    ev.preventDefault()
+      let li = document.createElement('li')
+      li.classList = 'nods'
+      let rowid = 'list-' + cont
+      cont ++
+    
+
+      li.appendChild(creatLabel('NameCandidato', 'Nome :')) 
+      li.appendChild(creatInput(rowid, '' ))
+
+      li.appendChild(creatLabel('experiencia', '  experiência :'))
+      let id1 = ' -0.2'
+      li.appendChild(creatInput(id1,  id1, 'exp-'+ cont, undefined,  'radio'))
+      li.appendChild(creatLabel('experiencia', '  0-2 anos :'))
+
+      let id2 = ' -3.4'  
+      li.appendChild(creatInput(id2,  id2 ,'exp-'+ cont, undefined, 'radio'))
+      li.appendChild(creatLabel('experiencia', ' 3-4 anos :'))
+      
+      let id3 =  ' -.5'  
+      li.appendChild(creatInput(id3,  id3 ,'exp-'+ cont, undefined, 'radio')) 
+      li.appendChild(creatLabel('experiencia', ' + 5 anos : '))
+      
+     
+
+    let  buttonrmv = document.createElement('button')
+     buttonrmv.innerText = 'Remover'
+     buttonrmv.type = 'button'
+     buttonrmv.id = 'remove'
+
+
+    
+
+     li.appendChild(buttonrmv)
+     arealist.append(li , registerDev)
+
+
+
+   let btnRemove =  document.querySelectorAll("#remove")
+    
+   btnRemove.forEach(function(btn){
+    btn.addEventListener('click', function(ev){
+         let listas = ev.currentTarget.parentNode
+         listas.remove()
+       
+    })
+    
+     
+   })
+
+
+
+    document.getElementById("registerDev").addEventListener("click", function(ev){
+        ev.preventDefault()
+        let nome = document.getElementById('nomeDev').value
+        document.querySelectorAll('.nods').forEach(function(row){
+           
+            let listerMarqued =  {
+                NameDev:nome,
+                NameTecnologia:row.querySelector('input[type="text"]').value,
+                experiEncia:row.querySelector('input[type="radio"]:checked').value + 'anos'
+              
+            }
+
+          devs.push(listerMarqued)
+
+        })
+        console.log(devs)
+        
+    })
+
+
+    
+
+})
+
+
+  
+
+
+
+
+
+
+
+
+
+
